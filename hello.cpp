791c5692bec9613bcff4b7a6c4948a8afbd81780
pdralston <ralstontech@gmail.com>
2018-03-21 18:20:06 -0700

    Created Hello World in C++

---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+  std::cout << "Hello World!\nMy name is David Ralston.\nIt is a pleasure to meet your acquaintence."
+  return 0;
+}
